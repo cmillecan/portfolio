1c247cf016a281ef7bab7c333193a68d6afd64be
cmillecan <cmillecan@gmail.com>
2021-06-07 14:21:45 -0700

    Updates

---
+++ precache-manifest.6dcc2a558cc30ee81dc8927642bd82c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0eb4d7385d48fa47e342",
+    "url": "/portfolio/static/css/main.16e785ab.chunk.css"
+  },
+  {
+    "revision": "0eb4d7385d48fa47e342",
+    "url": "/portfolio/static/js/main.7082f66e.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "0c8a69d4058209b8b64f",
+    "url": "/portfolio/static/js/2.99ba25b9.chunk.js"
+  },
+  {
+    "revision": "a3e60d8ef93356af152e45114e225e25",
+    "url": "/portfolio/static/media/cmkoreansymbol.a3e60d8e.svg"
+  },
+  {
+    "revision": "6c9694849cbe136a5619512f68d84cc4",
+    "url": "/portfolio/index.html"
+  }
+];
