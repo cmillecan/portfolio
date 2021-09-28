e95dd60307c95675efe5f09faef528ecf6a5b1dc
cmillecan <cmillecan@gmail.com>
2021-09-28 16:47:08 -0700

    Updates

---
+++ precache-manifest.49627bbc9b4dbac1443cddc711429046.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d362aa26dbde5675807d",
+    "url": "/static/css/main.0365f729.chunk.css"
+  },
+  {
+    "revision": "d362aa26dbde5675807d",
+    "url": "/static/js/main.16b22e37.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d8df1d1a15da13c1b257",
+    "url": "/static/css/2.286c7af6.chunk.css"
+  },
+  {
+    "revision": "d8df1d1a15da13c1b257",
+    "url": "/static/js/2.199b3f22.chunk.js"
+  },
+  {
+    "revision": "f69516f61f3ff9ad6bcecf9401a89191",
+    "url": "/static/media/portfolio-blog.f69516f6.png"
+  },
+  {
+    "revision": "b111a3e1629543967e169329932f0c8e",
+    "url": "/static/media/portfolio-fitapp.b111a3e1.png"
+  },
+  {
+    "revision": "46278fd9b54a367fe8a09777a4c3bb84",
+    "url": "/static/media/portfolio-disney.46278fd9.png"
+  },
+  {
+    "revision": "3191cac281162c649b2f9e3982bccfdb",
+    "url": "/static/media/portfolio-music.3191cac2.png"
+  },
+  {
+    "revision": "acf79062c0433431862b78c9d045e6bc",
+    "url": "/static/media/logo.acf79062.png"
+  },
+  {
+    "revision": "c588b41f06295ae71d6e86cb6b1f8576",
+    "url": "/static/media/heroimg2.c588b41f.png"
+  },
+  {
+    "revision": "ed6a5e47212665c94070ed65b86e9cb8",
+    "url": "/static/media/portfolio-calculator.ed6a5e47.png"
+  },
+  {
+    "revision": "4a65e87ca1c4f244f1d446e37112799a",
+    "url": "/static/media/portfolio-memory.4a65e87c.png"
+  },
+  {
+    "revision": "03bad5e5f7d5241c3312510243dc9ba9",
+    "url": "/index.html"
+  }
+];
