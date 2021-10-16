3f211cf152a02ac00c526183ba02092b2738d51a
cmillecan <cmillecan@gmail.com>
2021-10-16 10:40:51 -0700

    update about me section

--- src/components/About.js
+++ src/components/About.js
@@ -23,38 +23,35 @@ const About = () => {
   return (
     <Section id="about">
       <div className="about-div">
+        <div className="name">
+          <h3>
+            Hi, I'm <strong>Connie</strong>!
+          </h3>
+        </div>
         <div className="about-box">
-          <div className="about-left">
-            <div className="image-container">
-              {/*<div className="image-border">*/}
-              <img
-                className="about-pic"
-                src="https://i.imgur.com/DOrIxZr.jpg"
-                alt="headshot"
-              />
-              {/*</div>*/}
-            </div>
-          </div>
-          <div className="about-right">
-            <div className="name">
-              <h3>
-                Hi, I'm <strong>Connie</strong>
-              </h3>
+          <div className="about-me-paragraph">
+            <div className="about-pic">
+              <img src="https://i.imgur.com/DOrIxZr.jpg" alt="headshot" />
             </div>
             <p>
               I am a software engineer and musician passionate about creativity.
               I enjoy full-stack development and am especially enthusiastic
-              about front-end development.
+              about building beautiful and intuitive UIs on the front end.
             </p>
-            <br />
             <p>
-              As a software engineer, I'm always focused on how code feels,
-              looks, and functions but am also detail-oriented about visual
-              elements. I have enjoyed working on full-stack projects and
-              learning how the front-end and back-end communicate with each
-              other.
+              I had my first experience with web design at age 14 when I built
+              my own website as a hobby. I loved spending hours designing and
+              making improvements, and even ended up making a little money from
+              it! Like music, it was another creative outlet for me and I
+              continued to maintain my website for years.
             </p>
-            <br />
+            <p>
+              After graduating from music school, my spark was re-ignited as I
+              was establishing my private teaching studio. It was the perfect
+              excuse to make another website! This brought back the many fun
+              hours I'd spent in the past and reminded me how there was always
+              more that I wanted to learn.
+            </p>
             <p>
               What excites me the most about software engineering is that
               learning is never-ending and the small challenges that I
