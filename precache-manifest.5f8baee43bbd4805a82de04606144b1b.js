3a0588a970ff3b66fc4dd4a81270cd749cc33cc6
cmillecan <cmillecan@gmail.com>
2023-12-28 10:42:34 -0800

    Updates

---
+++ precache-manifest.5f8baee43bbd4805a82de04606144b1b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c7d67964d473d037f8d",
+    "url": "/portfolio/static/css/main.22293c71.chunk.css"
+  },
+  {
+    "revision": "6c7d67964d473d037f8d",
+    "url": "/portfolio/static/js/main.7ffb050e.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "1222282a57edef15842a",
+    "url": "/portfolio/static/js/2.24a35f36.chunk.js"
+  },
+  {
+    "revision": "b111a3e1629543967e169329932f0c8e",
+    "url": "/portfolio/static/media/portfolio-fitapp.b111a3e1.png"
+  },
+  {
+    "revision": "46278fd9b54a367fe8a09777a4c3bb84",
+    "url": "/portfolio/static/media/portfolio-disney.46278fd9.png"
+  },
+  {
+    "revision": "3191cac281162c649b2f9e3982bccfdb",
+    "url": "/portfolio/static/media/portfolio-music.3191cac2.png"
+  },
+  {
+    "revision": "328d03184aefe3b73a63dd1b3de82df1",
+    "url": "/portfolio/static/media/pic.328d0318.png"
+  },
+  {
+    "revision": "91a00944c73385a337e7c1097d18eb31",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.336558c9b702f0a540bc92ca6f760d01.js"
+  "/portfolio/precache-manifest.5f8baee43bbd4805a82de04606144b1b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
