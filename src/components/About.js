4d5759d467ae546cab5ce1580912a04efb4a7393
cmillecan <cmillecan@gmail.com>
2024-01-02 14:17:13 -0800

    edit about section

--- src/components/About.js
+++ src/components/About.js
@@ -26,12 +26,12 @@ const About = () => {
                 <h5>About</h5>
                 </div>
                 <p>
-                    Hey there! I'm a software engineer with experience in agile environments, primarily working with React and SaaS applications. I have a deep passion for frontend development, where I focus on crafting clean, efficient code and prioritizing user-friendly experiences.
+                    Hello! I'm a software engineer with experience in agile environments, primarily working with React and SaaS applications. I have a deep passion for frontend development, where I focus on crafting clean, efficient code and prioritizing user-friendly experiences.
                 </p>
                 <p>
                     But here's a fun twist - I'm also a musician! This part of my life has been a huge influence on how I approach software engineering. It's where I learned the magic of blending creativity with discipline and the importance of sweating the small stuff. These skills are key assets in my coding and design work, helping me create apps that are functional and intuitive.                </p>
                 <p>
-                    What really excites me about software engineering is its constant evolution – it's a field where learning never stops, and there's always a new challenge just around the corner. With every project I dive into, I get to blend technical skill with creativity, aiming to create impactful and user-centric solutions.
+                    What really excites me about software engineering is its constant evolution – it's a field where learning never stops, and there's always a new challenge just around the corner. With every project I dive into, I get to combine technical skill with creativity, aiming to create impactful and user-centric solutions.
                 </p>
             </div>
         </Section>
